67f242380f24ad49efc50cb6bfe3e92c2bb9ec16
muhamaul <122138137+muhamaul@users.noreply.github.com>
2023-01-07 10:43:42 +0700

    Create satpam.java

---
+++ satpam.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MacbookMAX
+ */
+public class satpam extends pegawai{
+
+public void menampilkan()
+
+{
+
+System.out.println("Nama : "+nama);
+
+System.out.println("Id Pegawai : "+id_pegawai);
+
+System.out.println("Gaji : "+gaji);
+
+}
+
+public void tugas(){
+
+System.out.println("Tugas : Menjaga keamanan didalam dan diluar frenchise");
+
+System.out.println("----------------------------------------------");
+
+}
+
+}

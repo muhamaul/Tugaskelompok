b854576a188d875b5c2412a0aa3976ab739c78a7
muhamaul <122138137+muhamaul@users.noreply.github.com>
2023-01-07 10:50:41 +0700

    Create koki.java

---
+++ koki.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MacbookMAX
+ */
+public class koki extends pegawai{
+
+@Override
+
+public void menampilkan()
+
+{
+
+System.out.println("Nama : "+nama);
+
+System.out.println("Id Pegawai : "+id_pegawai);
+
+System.out.println("Gaji : "+gaji);
+
+}
+
+public void tugas(){
+
+System.out.println("Tugas : Memasak makanan dan Membuat minuman");
+
+System.out.println("----------------------------------------------");
+
+}
+
+}

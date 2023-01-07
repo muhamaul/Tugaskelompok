1d24e39a89107df3376370b3424263a9a1f52049
muhamaul <122138137+muhamaul@users.noreply.github.com>
2023-01-07 10:44:22 +0700

    Create kasir.java

---
+++ kasir.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MacbookMAX
+ */
+public class kasir extends pegawai{
+
+@Override
+
+public void menampilkan()
+
+{
+
+System.out.println("Nama : "+nama);
+
+System.out.println("Id Pegawai : "+id_pegawai);
+
+System.out.println("Gaji : "+gaji);
+
+}
+
+public void tugas(){
+
+System.out.println("Tugas : Melakukan transaksi dengan pembeli");
+
+System.out.println("----------------------------------------------");
+
+}
+
+}

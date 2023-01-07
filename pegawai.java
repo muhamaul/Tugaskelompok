f8adcab490d38415f7b8d452a8181ed106ac7184
muhamaul <122138137+muhamaul@users.noreply.github.com>
2023-01-07 10:41:05 +0700

    Create pegawai.java

---
+++ pegawai.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MacbookMAX
+ */
+public class pegawai {
+    String nama ;
+    int id_pegawai;
+    String gaji;
+
+public void menampilkan(){
+
+System.out.println("Menampilkan nama, id pegawai, gaji dan tugas.");
+
+System.out.println("------------------------------------------------------");
+
+}
+
+}
